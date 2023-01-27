fcc10c6c3273afd01bd7ba61666860a98ee22cc5
sadecebatu0 <topcubatuhan00@gmail.com>
2023-01-27 20:01:39 +0300

    katmanların olusturulması - entity katmanı - data access - repository altyapısı - context - business - arayüz - viewComponent - queryString

---
+++ MvcWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using MvcWebUI.Models;
+
+namespace MvcWebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+
+        private ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+    }
+}
